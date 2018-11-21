f0cbfe72b5d7d2514734751487cc48adc8859c00
杨黄林 <yhl452493373@gmail.com>
2018-11-21 10:18:13 +0800

    设置freemarker模板和资源文件路径从配置文件中读取

--- src/main/java/com/github/yhl452493373/config/DefaultViewConfig.java
+++ src/main/java/com/github/yhl452493373/config/DefaultViewConfig.java
@@ -1,12 +1,17 @@
 package com.github.yhl452493373.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class DefaultViewConfig implements WebMvcConfigurer {
+    @Value("${spring.resources.static-locations:classpath:/static/}")
+    private String[] resourceLocations;
+
     /**
      * 默认首页
      */
@@ -14,5 +19,13 @@ public class DefaultViewConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("forward:/index");
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+
+    /**
+     * 资源文件默认映射到/static/路径下
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations(resourceLocations);
     }
 }
--- src/main/java/com/github/yhl452493373/config/FreemarkerConfig.java
+++ src/main/java/com/github/yhl452493373/config/FreemarkerConfig.java
@@ -2,6 +2,7 @@ package com.github.yhl452493373.config;
 
 import com.jagregory.shiro.freemarker.ShiroTags;
 import freemarker.template.TemplateException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
@@ -10,10 +11,13 @@ import java.io.IOException;
 
 @Configuration
 public class FreemarkerConfig {
+    @Value("${spring.freemarker.template-loader-path:classpath:/templates/}")
+    private String templateLoaderPath;
+
     @Bean
     public FreeMarkerConfigurer freeMarkerConfigurer() throws IOException, TemplateException {
         FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
-        freeMarkerConfigurer.setTemplateLoaderPath("classpath:templates/");
+        freeMarkerConfigurer.setTemplateLoaderPath(templateLoaderPath);
         freemarker.template.Configuration configuration = freeMarkerConfigurer.createConfiguration();
         configuration.setDefaultEncoding("UTF-8");
         configuration.setSharedVariable("shiro" , new ShiroTags());
