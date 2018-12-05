0743f8a5b1b6b14a53a523123fa7f6e4c9e6cb18
杨黄林 <yhl452493373@gmail.com>
2018-12-05 13:50:55 +0800

    freemarker增加对localdatetime的支持

--- src/main/java/com/github/yhl452493373/config/FreemarkerConfig.java
+++ src/main/java/com/github/yhl452493373/config/FreemarkerConfig.java
@@ -1,13 +1,19 @@
 package com.github.yhl452493373.config;
 
 import com.jagregory.shiro.freemarker.ShiroTags;
-import freemarker.template.TemplateException;
+import freemarker.template.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Configuration
 public class FreemarkerConfig {
@@ -20,8 +26,29 @@ public class FreemarkerConfig {
         freeMarkerConfigurer.setTemplateLoaderPath(templateLoaderPath);
         freemarker.template.Configuration configuration = freeMarkerConfigurer.createConfiguration();
         configuration.setDefaultEncoding("UTF-8");
-        configuration.setSharedVariable("shiro" , new ShiroTags());
+        configuration.setSharedVariable("shiro", new ShiroTags());
+        configuration.setObjectWrapper(new CustomObjectWrapper());
         freeMarkerConfigurer.setConfiguration(configuration);
         return freeMarkerConfigurer;
+    }
+
+    @SuppressWarnings("deprecation")
+    private class CustomObjectWrapper extends DefaultObjectWrapper {
+        @Override
+        public TemplateModel wrap(Object obj) throws TemplateModelException {
+            if (obj instanceof LocalDateTime) {
+                Timestamp timestamp = Timestamp.valueOf((LocalDateTime) obj);
+                return new SimpleDate(timestamp);
+            }
+            if (obj instanceof LocalDate) {
+                Date date = Date.valueOf((LocalDate) obj);
+                return new SimpleDate(date);
+            }
+            if (obj instanceof LocalTime) {
+                Time time = Time.valueOf((LocalTime) obj);
+                return new SimpleDate(time);
+            }
+            return super.wrap(obj);
+        }
     }
 }
